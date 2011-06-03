4580ed2fb5a96b3ab2b14c3743202ab95f2ff062
Brian Schalme <bschalme@airspeed.ca>
2011-06-03 06:27:25 -0500

    Added NoOpDAO. Began work on TSheetsService

---
+++ src/main/groovy/ca/airspeed/timesheet/service/TSheetsService.groovy
@@ -0,0 +1,63 @@
+/*
+    Copyright 2011 Airspeed Consulting
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+package ca.airspeed.timesheet.service
+
+import ca.airspeed.timesheet.dao.ITimesheetDAO
+import ca.airspeed.control.IControlDAO
+import ca.airspeed.canonical.Control
+import ca.airspeed.canonical.TimesheetEntry
+
+import org.apache.commons.lang.NotImplementedException
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.format.DateTimeFormatter
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+
+class TSheetsService implements ITimesheetService {
+	private static final String YMD = "yyyy-MM-dd"
+	private static final String LAST_FETCHED = "timesheet.last.fetch.date"
+	
+	@Autowired
+	ITimesheetDAO timesheetDao
+	
+	@Autowired
+	IControlDAO controlDao
+	
+	TimesheetEntry[] create(TimesheetEntry[] entries) {
+		throw new NotImplementedException()
+	}
+	
+	TimesheetEntry[] read(Date from, Date to) {
+		throw new NotImplementedException()
+	}
+	
+	TimesheetEntry[] fetchNewTimesheetEntries() {
+		Control ctrl = controlDao.read(LAST_FETCHED)
+		DateTimeFormatter fmt = DateTimeFormat.forPattern(YMD)
+		DateTime lastFetchedDT = fmt.parseDateTime(ctrl.value)
+		String yesterdayStr = fmt.print(new DateTime().minusDays(1))
+		DateTime yesterday = fmt.parseDateTime(yesterdayStr)
+		
+		def results =  timesheetDao.read(lastFetchedDT.plusDays(1).toDate(), yesterday.toDate())
+		ctrl.value = yesterdayStr
+		controlDao.update(ctrl)
+		return results
+	}
+}
---
+++ src/test/groovy/ca/airspeed/timesheet/service/TestTSheetsService.groovy
@@ -0,0 +1,65 @@
+/*
+Copyright 2011 Airspeed Consulting
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+package ca.airspeed.timesheet.service
+
+import java.text.SimpleDateFormat
+
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.format.DateTimeFormatter
+import static org.junit.Assert.*
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ca.airspeed.timesheet.dao.ITimesheetDAO
+import ca.airspeed.canonical.Control
+import ca.airspeed.canonical.TimesheetEntry
+import ca.airspeed.control.IControlDAO
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.easymock.EasyMock.*
+
+class TestTSheetsService {
+	def mockControl = createStrictControl()
+	
+	@Test
+	void testFetchTwoDays() {
+		TSheetsService service = new TSheetsService()
+		def mockTimesheetDao = mockControl.createMock(ITimesheetDAO.class)
+		def mockControlDao = mockControl.createMock(IControlDAO.class)
+		service.setTimesheetDao(mockTimesheetDao)
+		service.setControlDao(mockControlDao)
+
+		DateTime now = new DateTime()
+		DateTime yesterday = now.minusDays(1)
+		DateTime twoDaysAgo = now.minusDays(2)
+		DateTime threeDaysAgo = now.minusDays(3)
+		DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd")
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd")
+		def dtYesterday = sdf.parse(fmt.print(yesterday))
+		def dtTwoDaysAgo = sdf.parse(fmt.print(twoDaysAgo))
+		
+		Control oldControl = new Control(id:1, name:"timesheet.last.fetch.date", value:fmt.print(threeDaysAgo))
+		Control newControl = new Control(id:1, name:"timesheet.last.fetch.date", value:fmt.print(yesterday))
+		expect(mockControlDao.read("timesheet.last.fetch.date")).andReturn(oldControl)
+		expect(mockTimesheetDao.read(dtTwoDaysAgo, dtYesterday)).andReturn(new ArrayList<TimesheetEntry>())
+		expect(mockControlDao.update(newControl)).andReturn(newControl)
+		
+		mockControl.replay()
+		def timesheetEntries = service.fetchNewTimesheetEntries()
+		mockControl.verify()
+	}
+}
